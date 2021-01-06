f301bd359919a14ec3007692bac4dd0deb1aa482
Fellipe bandeira <76072907+fzta492@users.noreply.github.com>
2021-01-05 21:13:24 -0300

    continuacao aulas

---
+++ exercicio 16.py
@@ -0,0 +1,6 @@
+''' from math import trunc
+n = float(input('digite um numero:'))
+print('seu numero inteiro é {}'.format(trunc(n)))'''
+
+num = float(input(' digite um numero quebrado:'))
+print(' o numero digitado é {} e seu numero inteiro é {}'.format(num, int(num)))
